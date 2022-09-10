c8d30af50849eb38f3aa2b001674016646f29473
Ashish Nimrot <ashishnimrot@gmail.com>
2022-09-11 00:11:13 +0530

    create express server and database coannection

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+    console.log('connected to MongoDB')
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+
+app.listen(8800, () => {
+    console.log('Backened sever is running')
+});
