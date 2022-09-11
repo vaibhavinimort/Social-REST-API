5714d8f2900fd32d8d34d83bc679736a2343e47a
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-11 23:30:45 +0530

    added auth and user route on main route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -14,8 +16,10 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
 //middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan('common'));
+app.use(morgan("common"));
 
+app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute)
 
 app.listen(8800, () => {
     console.log('Backened sever is running')
