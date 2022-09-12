28f5aaa7577ad933c5bac975959b178a0dc495ae
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-12 17:30:44 +0530

    created register api

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,23 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt =
 
-router.get("/", (req, res) => {
-    res.send("hey its auth router")
-})
+    //Resgister
+    router.post("/register", async(req, res) => {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        // const newUser = new User({ username, email, password } = req.body);
+        try {
+            const user = await newUser.save();
+            res.status(200).json(user);
+        } catch (err) {
+            console.log(err)
+        }
+
+    });
 
 module.exports = router
