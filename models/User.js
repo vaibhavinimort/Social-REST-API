c2773ae5a028a4c8055ae6741e0b8e08214c8dc9
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-14 00:29:34 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,44 +1,62 @@
    const mongoose = require("mongoose")
 
    const userSchema = new mongoose.Schema({
-       username: {
-           type: String,
-           require: true,
-           min: 3,
-           max: 20,
-           unique: true
+           username: {
+               type: String,
+               require: true,
+               min: 3,
+               max: 20,
+               unique: true
+           },
+           email: {
+               type: String,
+               required: true,
+               min: 6
+           },
+           password: {
+               type: String,
+               required: true,
+               min: 6
+           },
+           profilePicture: {
+               type: String,
+               default: ""
+           },
+           coverPicture: {
+               type: String,
+               default: ""
+           },
+           followers: {
+               type: Array,
+               default: ""
+           },
+           followins: {
+               type: Array,
+               default: []
+           },
+           isadmin: {
+               type: Boolean,
+               default: false,
+           },
+           desc: {
+               type: String,
+               max: 50
+           },
+           city: {
+               type: String,
+               max: 50
+           },
+           from: {
+               type: String,
+               max: 50
+           },
+           relationship: {
+               type: Number,
+               enum: [1, 2, 3]
+           }
        },
-       email: {
-           type: String,
-           required: true,
-           min: 6
-       },
-       password: {
-           type: String,
-           required: true,
-           min: 6
-       },
-       profilePicture: {
-           type: String,
-           default: ""
-       },
-       coverPicture: {
-           type: String,
-           default: ""
-       },
-       followers: {
-           type: Array,
-           default: ""
-       },
-       followins: {
-           type: Array,
-           default: []
-       },
-       isadmin: {
-           type: Boolean,
-           default: false,
-       },
-   }, { timestamp: true });
+
+       { timestamp: true });
 
 
    module.exports = mongoose.model("User", userSchema);
