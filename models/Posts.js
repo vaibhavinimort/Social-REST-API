baf6bf043b96ce261bf7cd287107759352cb4354
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-16 00:32:34 +0530

    created post file in models

---
+++ models/Posts.js
@@ -0,0 +1,25 @@
+   const mongoose = require("mongoose")
+
+   const PostSchema = new mongoose.Schema({
+           userId: {
+               type: String,
+               required: true
+           },
+           desc: {
+               type: String,
+               max: 500,
+           },
+           img: {
+               type: String,
+               max: 500,
+           },
+           likes: {
+               type: Array,
+               default: [],
+           },
+       }, { timestamp: true }
+
+   );
+
+
+   module.exports = mongoose.model("Post", PostSchema);
