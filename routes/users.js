1f91f04038ccda54f688de980c6a9d33651a81b5
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-16 00:34:09 +0530

    created unfollow user api

--- routes/users.js
+++ routes/users.js
@@ -62,9 +62,9 @@ router.put("/:id/follow", async(req, res) => {
             const user = await User.findById(req.params.id);
             //login user requested to follower
             const currentUser = await User.findById(req.body.userId);
-            if (user.followers.includes(req.body.userId)) {
-                await user.updateOne({ $pill: { followers: req.body.userId } });
-                await currentUser.updateOne({ $pull: { followings: req.body.userId } });
+            if (!user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $push: { followers: req.body.userId } });
+                await currentUser.updateOne({ $push: { followings: req.body.userId } });
                 res.status(200).json("user has been followed");
             } else {
                 res.status(403).json("you already follow this user")
@@ -85,12 +85,12 @@ router.put("/:id/unfollow", async(req, res) => {
             const user = await User.findById(req.params.id);
             //login user requested to follower
             const currentUser = await User.findById(req.body.userId);
-            if (!user.followers.includes(req.body.userId)) {
-                await user.updateOne({ $push: { followers: req.body.userId } });
-                await currentUser.updateOne({ $push: { followings: req.body.userId } });
+            if (user.followers.includes(req.body.userId)) {
+                await user.updateOne({ $pull: { followers: req.body.userId } });
+                await currentUser.updateOne({ $pull: { followings: req.body.userId } });
                 res.status(200).json("user has been unfollowed");
             } else {
-                res.status(403).json("you dont follow this user")
+                res.status(403).json("you don't follow this user")
             }
         } catch (err) {
             res.status(500).json(err)
