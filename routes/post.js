3a3c3aad856a5ffd17e9aaa52021b6f4819aa4c6
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-16 00:33:19 +0530

    crreated routers post

--- routes/post.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { posts } = require('../mock');
-
-router.get("/", (req, res) => {
-    res.status(200).json(posts);
-})
-
-module.exports = router
