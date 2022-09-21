db17b823c556a1db79257b578e5bab6a2f2e5dba
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-21 17:41:27 +0530

    created api get users all post and modify timeline api

--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,10 @@ router.post("/", async(req, res) => {
     const newPost = new Post(req.body);
     try {
         const savedPost = await newPost.save();
+        console.log(savedPost)
         res.status(200).json(savedPost);
     } catch (err) {
+        console.log(err)
         res.status(500).json(err);
     }
 });
@@ -68,17 +70,27 @@ router.get("/:id", async(req, res) => {
 });
 
 //get timeline posts
-router.get("/timeline/all", async(req, res) => {
-    let postArray = [];
+router.get("/timeline/:userId", async(req, res) => {
     try {
-        const currentUser = await User.findById(req.body.userId);
+        const currentUser = await User.findById(req.params.userId);
         const userPosts = await Post.find({ userId: currentUser._id });
         const friendPosts = await Promise.all(
             currentUser.followings.map((friendId) => {
                 return Post.find({ userId: friendId });
             })
         );
-        res.json(userPosts.concat(...friendPosts));
+        res.status(200).json(userPosts.concat(...friendPosts));
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//get user's all posts
+router.get("/profile/:usermame", async(req, res) => {
+    try {
+        const user = await User.findOne({ username: req.params.username });
+        const posts = await Post.find({ userId: user._id });
+        res.status(200).json(posts);
     } catch (err) {
         res.status(500).json(err);
     }
