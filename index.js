11b2737a7bf9ffdd077058530bf18bc16a25a02c
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-23 00:30:16 +0530

    added multer npm and dirname path and upload file api

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const cors = require('cors');
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 
@@ -15,11 +17,31 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
     console.log('connected to MongoDB')
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cors());
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.query.name);
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+        return res.status(200).json("File uploded successfully");
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 app.use("/api/users", userRoute)
 app.use("/api/auth", authRoute)
