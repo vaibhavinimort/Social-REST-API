95ca2dbc484532ef02f936b595dc245d8beca1d3
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-25 23:50:21 +0530

    add api route conversations & messages

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const morgan = require('morgan');
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const conversationRoute = require("./routes/conversations");
+const messageRoute = require("./routes/messages");
 const cors = require('cors');
 const multer = require("multer");
 const path = require("path");
@@ -43,9 +45,11 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
     }
 });
 
-app.use("/api/users", userRoute)
-app.use("/api/auth", authRoute)
-app.use("/api/posts", postRoute)
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
     console.log('Backened sever is running')
