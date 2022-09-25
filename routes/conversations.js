1ce51e0f1fed8299478859519c2c44fb79a4f541
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-26 00:02:10 +0530

    create get conv of user api

--- routes/conversations.js
+++ routes/conversations.js
@@ -17,13 +17,16 @@ router.post("/", async(req, res) => {
 });
 
 //get conv of user
-// router.gert("/:userId",async(req,res)=>{
-//     try{
-
-//     }catch(err){
-//     res.status(500).json(err);
-//     }
-// })
+router.get("/:userId", async(req, res) => {
+    try {
+        const conversation = await Conversation.find({
+            members: { $in: [req.params.userId] },
+        });
+        res.status(200).json(conversation);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
