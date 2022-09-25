8dd8b5af9a5a78de6646df41ef83fe70d9579635
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-26 00:17:46 +0530

    create add new messages api

--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
 const Message = require("../models/Message");
 
+//add 
+
+router.post("/", async(req, res) => {
+    const newMessage = new Message(req.body);
+    try {
+        const savedMessage = await newMessage.save();
+        res.status(200).json(savedMessage);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
