cf1d278a20b7943a9a24a3bf2c17165902ec23cb
vaibhavinimrot@gmail.com <113259829+vaibhavinimort@users.noreply.github.com>
2022-09-26 23:44:46 +0530

    create get message api

--- routes/messages.js
+++ routes/messages.js
@@ -12,4 +12,16 @@ router.post("/", async(req, res) => {
         res.status(500).json(err);
     }
 });
+
+//get
+router.get("/:conversationId", async(req, res) => {
+    try {
+        const messages = await Message.find({
+            conversationId: req.params.conversationId,
+        });
+        res.status(200).json(messages);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 module.exports = router;
